32a17d1cd40a1a1e48540ce6ac89419c22e080c8
Tristan Stevens <tvut@outlook.com>
2022-11-25 10:08:52 -0500

    resize input image

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 from flask_cors import CORS
 
+import tensorflow as tf
+import numpy as np
+import keras
+from keras.preprocessing import image
+
 app = Flask(__name__)
 cors = CORS(app, resources={r'/*': {"origins": '*'}})
 # app.config['CORS_HEADER'] = 'Content-Type'
@@ -10,8 +15,10 @@ cors = CORS(app, resources={r'/*': {"origins": '*'}})
 def upload_file():
     formData = request.files.get('file','')
     print(type(formData))
-    formData.save('image.jpg')
-    print(formData)
+    img = formData.load_img(formData, target_size(128, 128))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    print(x.shape)
     # print(request.file)
     # uploaded_file = request.files['file']
     # if uploaded_file.filename != '':
